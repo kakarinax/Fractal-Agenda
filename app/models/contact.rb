1c4b1c40a66a8be8e0aa7f27527f94f12ad3bfd7
Karina Almeida <s.karinaalmeida@gmail.com>
2022-05-28 01:48:19 -0300

    updated both models

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,5 +3,7 @@ class Contact < ApplicationRecord
   validates :name, presence: true
   validates :relationship, presence: true
 
+  validates :name, :user_id, presence: true
+
   enum relationship: { personal: 'personal', professional: 'professional' }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :contacts
+  has_many :contacts, dependent: :destroy
 
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
