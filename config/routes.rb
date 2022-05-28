4da6dd4c647eca03755df42328d3f35c4b962677
Karina Almeida <s.karinaalmeida@gmail.com>
2022-05-28 01:47:11 -0300

    added sign up route and improved routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   devise_for :users
   resources :contacts
-  resources :users
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new'
 end
