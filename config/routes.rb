274f6d222365efded17b93cf10834fcbee7eedf4
Karina Almeida <s.karinaalmeida@gmail.com>
2022-05-30 01:44:01 -0300

    routes updates and sessions test generate

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   root 'pages#home'
   devise_for :users
   resources :contacts
-  resources :users, except: [:new]
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'signup', to: 'users#new'
 end
